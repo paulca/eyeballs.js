53d3405dda0972f3b38301ae0a1239e722e23694
Paul Campbell <paul@rslw.com>
2010-05-15 15:09:02 +0100

    well, now, there's a working implementation

--- models/review.js
+++ models/review.js
@@ -1,4 +1,5 @@
-var review = o_O(function(){
-  // validates_presence_of('title');
-  // validates_presence_of('content');
-});
+// var review = o_O(function(){
+//   console.log(that.validates_presence_of)
+//   // validates_presence_of('title');
+//   // validates_presence_of('content');
+// });
--- o_O.js
+++ o_O.js
@@ -1,7 +1,54 @@
-var o_O = function(callback){
-  return function(){
-    return arguments[0];
+var o_O = function(attributes){
+  var that = {
+    validates_presence_of: function(field){
+      this.validations.presence.push(field)
+    },
+    save: function(){
+      for(i = 0; i < this.validations.presence.length; i++)
+      {
+        var field = this.validations.presence[i];
+        if(this[field] == '' || this[field] == null)
+        {
+         this.errors.push(field + " should be present")
+        }
+      }
+      
+      if(this.errors.length == 0)
+      {
+        return true;
+      }
+      else
+      {
+        return false;
+      }
+    },
+    errors: [],
+    validations: {presence:[]}
+  };
+
+  for ( var attribute in attributes ) {
+    that[attribute] = attributes[attribute];
   }
+  
+  return that;
+  
+  // var initializer = {
+  //   validates_presence_of: function(field){
+  //     validations.push({field: field, type: 'presence'})
+  //   }
+  // }
+  // 
+  // callback(initializer);
+  // 
+  // return function(){
+  //   var validations;
+  //   var attributes = arguments[0] || {};
+  //   attributes.errors = []
+  //   attributes.save = function(){
+  //     return true;
+  //   }
+  //   return attributes;
+  // };
   // var validations;
   // validations = {presence:[]};
   // 
