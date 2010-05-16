672838a6be90c8824da62220ae0362256a06a6f6
Paul Campbell <paul@rslw.com>
2010-05-16 21:33:58 +0100

    this is pretty amazing

--- controllers/reviews_controller.js
+++ controllers/reviews_controller.js
@@ -10,10 +10,19 @@ var ReviewsController = {
     }
     else
     {
-      console.log(review.errors);
+      console.log('ah! ah! ah!');
+    }
+  },
+  destroy: function(){
+    if(confirm('Are You sure?'))
+    {
+      $(this).parents('div.review:first').remove();
     }
   }
 }
 
-$('form[data-action=reviews-create]').submit(ReviewsController.create);
-$('form[data-action=reviews-create]').submit(function(){ return false });
+$('form[data-action=reviews-create]').live('submit',ReviewsController.create);
+$('form[data-action=reviews-create]').live('submit', function(){ return false });
+
+$('a[data-action=reviews-destroy]').live('click',ReviewsController.destroy);
+$('a[data-action=reviews-destroy]').live('click', function(){ return false });
