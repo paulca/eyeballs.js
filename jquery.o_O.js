1fc5717158ee7b3bac5f07e09e0226a8e68e0cbd
Paul Campbell <paul@rslw.com>
2010-05-20 23:42:37 +0100

    I can bind multiple events with the bestest shorthand in otwn

--- jquery.o_O.js
+++ jquery.o_O.js
@@ -25,17 +25,21 @@ o_O.controller = {
         })
       }
       $('[data-bind]').livequery(function(){
-        var binders = $(this).attr('data-bind').match(/[\+]?[^ :]+[: ]+[^ \/]+[ \/]+[^ ;]+[ ;]?/g)
-        if(binders.length > 0)
+        var binders = $(this).attr('data-bind').match(/[\+]?(\s+)?[^ :]?[: ]?[^ \/]+[ \/]+[^ ;]+[ ;]?/g)
+        if(binders != null && binders.length > 0)
         {
           for(i = 0; i < binders.length; i++)
           {
             var rule = binders[i];
-            var parts = rule.match(/([\+]?)([^ :]+)[: ]+([^ \/]+)[ \/]+([^ ;]+)[ ;]?/);
+            var parts = rule.match(/([\+])?(\s+)?(([^ :]+)([: ]+))?([^ \/]+)[ \/]+([^ ;]+)[ ;]?/)
             var default_bit = parts[1];
-            var this_action_event = parts[2];
-            var this_controller_name = parts[3];
-            var this_action = parts[4];
+            var this_action_event = parts[4];
+            if(this_action_event === undefined)
+            {
+              this_action_event = ($(this).is('form')) ? 'submit' : 'click';
+            }
+            var this_controller_name = parts[6];
+            var this_action = parts[7];
             if(this_controller_name == controller_name)
             {
               $(this).bind(this_action_event,controller[this_action]);
