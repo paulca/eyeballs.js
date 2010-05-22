ddb4ff835e7cb4d2fe81397bb35aaec0e1cd2f9d
Paul Campbell <paul@rslw.com>
2010-05-22 18:40:47 +0100

    got all docs working, now all I need is delete and I and I can make the meview couchapp

--- public/jquery.o_O.couchdb.js
+++ public/jquery.o_O.couchdb.js
@@ -2,6 +2,46 @@
 o_O.couchdb = {
   all: function(model){
     
+    var database = o_O.model.adapter.settings.database;
+    
+    var parts = [ 
+      database, '_design', model.table_name
+      ]
+    
+    var ddoc_url = '/' + parts.join('/');
+    var all_url = '/' + parts.join('/') + '/_view/all';
+    var response = $.ajax({url: all_url, type: 'GET', async: false}).responseText;
+    var responseObject = JSON.parse(response);
+    if(responseObject.error === 'not_found')
+    {
+      // create the doc
+      var design_doc = {
+         "_id": "_design/reviews",
+         "language": "javascript",
+         "views": {
+             "all": {
+                 "map": "function(doc) {\n  if(doc.model_name === 'Review')\n    emit(null, doc);\n}"
+             }
+         }
+      }
+      $.couch.db(database).saveDoc(design_doc)
+      // $.ajax({type: 'PUT', data: JSON.stringify(design_doc), aync: false, url: ddoc_url});
+      var response = $.ajax({url: all_url, type: 'GET', async: false}).responseText;
+    }
+    else
+    {
+      var documents = JSON.parse(response).rows;
+    }
+    
+    var documents = JSON.parse(response).rows;
+        var all_documents = []
+        for(var i = 0; i < documents.length; i++)
+        {
+          var document = documents[i];
+          document.value.id = document.id
+          all_documents.push(document.value);
+        }
+    return all_documents;
   },
   destroy: function(object){
     
