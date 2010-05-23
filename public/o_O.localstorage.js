661066293f37f4bd9a4a9b17b70abdb786823e38
Paul Campbell <paul@rslw.com>
2010-05-23 20:55:42 +0100

    make localstorage more functional

--- public/o_O.localstorage.js
+++ public/o_O.localstorage.js
@@ -2,24 +2,36 @@ if(typeof localStorage === 'object')
 {
   // We're storing locally, baby!
   o_O.localstorage = {
-    all: function(model){
+    all: function(model, callback){
       var all_objects = []
       for(var record in this.table(model))
       {
         all_objects.push(this.table(model)[record]);
       }
+      if(typeof callback === 'function')
+      {
+        callback(all_objects);
+      }
       return all_objects;
     },
-    destroy: function(object){
+    destroy: function(object, callback){
       var table = this.table(object)
       delete table[object.id]
       localStorage.setItem(object.table_name, JSON.stringify(table))
+      if(typeof callback === 'function')
+      {
+        callback(object)
+      }
     },
-    save: function(object)
+    save: function(object, callback)
     {
       var all = this.table(object)
       all[object.id] = object;
       localStorage.setItem(object.table_name, JSON.stringify(all))
+      if(typeof callback === 'function')
+      {
+        callback(object);
+      }
     },
     table: function(object)
     {
@@ -34,10 +46,14 @@ if(typeof localStorage === 'object')
         return JSON.parse(stored_all);
       }
     },
-    find: function(model, id)
+    find: function(model, id, callback)
     {
       var object = this.table(model)[id]
       object.id = id;
+      if(typeof callback === 'function')
+      {
+        callback(object);
+      }
       return object;
     }
   }
