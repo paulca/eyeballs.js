a279ef373feb3a78bf947c448d76ae5559ff8c16
Paul Campbell <paul@rslw.com>
2010-07-17 11:51:17 +0100

    remove comments

--- src/drivers/jquery/modules/o_O.controller.js
+++ src/drivers/jquery/modules/o_O.controller.js
@@ -22,7 +22,6 @@ o_O.controller = {
             }
             var this_controller_name = parts[6];
             var this_action = parts[7];
-            console.log(this_controller_name, controller_name, this_action_event, event.type);
             if(this_controller_name == controller_name && this_action_event === event.type)
             {
               controller[this_action].apply(this);
@@ -34,34 +33,6 @@ o_O.controller = {
           }
         }
       })
-      
-      // $('[data-bind]').livequery(function(){
-      //   var binders = $(this).attr('data-bind').match(/[\+]?(\s+)?[^ :]?[: ]?[^ #]+[ #]+[^ ;]+[ ;]?/g)
-      //   if(binders != null && binders.length > 0)
-      //   {
-      //     for(i = 0; i < binders.length; i++)
-      //     {
-      //       var rule = binders[i];
-      //       var parts = rule.match(/([\+])?(\s+)?(([^ :]+)([: ]+))?([^ #]+)[ #]+([^ ;]+)[ ;]?/)
-      //       var default_bit = parts[1];
-      //       var this_action_event = parts[4];
-      //       if(this_action_event === undefined)
-      //       {
-      //         this_action_event = ($(this).is('form')) ? 'submit' : 'click';
-      //       }
-      //       var this_controller_name = parts[6];
-      //       var this_action = parts[7];
-      //       if(this_controller_name == controller_name)
-      //       {
-      //         $(this).bind(this_action_event,controller[this_action]);
-      //         if(default_bit != '+')
-      //         {
-      //           $(this).bind(this_action_event, function(){ return false; });
-      //         }
-      //       }
-      //     }
-      //   }
-      // });
     });
     return controller;
   }
