4c4e23a76349b7d92949b6d040edfae2eac6f57b
Paul Campbell <paul@rslw.com>
2010-08-06 12:21:40 +0100

    remove initializer from plain files

--- templates/app_root/config/initializer.js
+++
@@ -1,8 +0,0 @@
-// This is a very handy place to put any initial logic
-// For example, choosing your adapter
-<% if rack_app? %>
-o_O.model.adapter = o_O.rest;
-o_O.config.authenticity_token = $('meta[name=csrf-token]').attr('content')
-<% else %>
-o_O.model.adapter = o_O.localstorage;
-<% end %>
