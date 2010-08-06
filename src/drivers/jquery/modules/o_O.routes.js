98913d1e5ac3a9f74795df7bb3426a5e2c751fb7
Paul Campbell <paul@rslw.com>
2010-08-06 12:14:00 +0100

    add params from the URL

--- src/drivers/jquery/modules/o_O.routes.js
+++ src/drivers/jquery/modules/o_O.routes.js
@@ -2,6 +2,7 @@ o_O.routes = {
   
   rules: {},
   urls: [],
+  regex_urls: [],
   
   figure_action: function(options){
     var parts = options.to.split('#');
@@ -22,15 +23,29 @@ o_O.routes = {
       },
       match: function(route, options){
         var parsed_route = route.o_O_trim('/')
+
         if(typeof prefix != 'undefined')
         {
           parsed_route = prefix + '/' + parsed_route
         }
-        o_O.routes.rules[parsed_route] = {
-          action: o_O.routes.figure_action(options),
-          with_args: options.with_args
-          };
-        o_O.routes.urls.push(parsed_route);
+
+        if(parsed_route.indexOf(':') >= 0)
+        { 
+          var regex_to_match = parsed_route.replace(/:[^\/]*/g, '([^\/]*)');
+          o_O.routes.rules[parsed_route] = {
+            action: o_O.routes.figure_action(options),
+            matchers: parsed_route.match(/:[^\/]*/g)
+            };
+          o_O.routes.regex_urls.push({regex: regex_to_match, matcher:parsed_route});
+        }
+        else
+        {
+          o_O.routes.rules[parsed_route] = {
+            action: o_O.routes.figure_action(options),
+            with_args: options.with_args
+            };
+          o_O.routes.urls.push(parsed_route);
+        }
       }
     }
   },
@@ -52,6 +67,31 @@ o_O.routes = {
         if(o_O.routes.urls.indexOf(hash) >= 0)
         {
           o_O.routes.rules[hash].action(o_O.routes.rules[hash].with_args);
+        }
+        else
+        {
+          for(i = 0; i < o_O.routes.regex_urls.length; i++)
+          {
+            if(match = hash.match(o_O.routes.regex_urls[i].regex))
+            {
+              var rule = o_O.routes.rules[o_O.routes.regex_urls[i].matcher];
+              rule.action(function(attr){
+                var params = {}
+                for(j = 0; j < rule.matchers.length; j++)
+                {
+                  params[rule.matchers[j].replace(/^:/, '')] = match[j+1];
+                }
+                if(attr)
+                {
+                  return params[attr];
+                }
+                else
+                {
+                  return params;
+                }
+              });
+            }
+          }
         }
       });
       
