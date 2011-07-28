bfe480c6353c865b335a3d2dbc713af019c300e5
Paul Campbell <paul@rslw.com>
2011-07-28 20:01:22 +0100

    jQuery-ize please!

--- eyeballs.jquery.js
+++ eyeballs.jquery.js
@@ -1,8 +1,8 @@
 eyeballs.dom_adapter = {
   initialize_collections: function(name){
     jQuery(document).ready(function(){
-      $('[data-collection=' + name + ']').each(function(index, item){
-        $(item).html(eyeballs.initialize(name).empty_collection())
+      jQuery('[data-collection=' + name + ']').each(function(index, item){
+        jQuery(item).html(eyeballs.initialize(name).empty_collection())
       })
     })
   }
